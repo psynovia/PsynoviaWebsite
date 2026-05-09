ec913ea1058f1f75d522a7fd06078c6ee13276cc
psynovia <tobiaswinner@psynovia.de>
2026-05-09 23:00:10 +0200

    Delete js directory

--- js/components/hero.js
+++
@@ -1,8 +0,0 @@
-window.Psynovia = window.Psynovia || {};
-window.Psynovia.renderHeroCard = function(block){
-  var el = window.Psynovia.el;
-  return el('section', { class: 'section heroCard', id: block.id },
-    el('h1', {}, block.headline),
-    el('p', {}, block.text)
-  );
-};
